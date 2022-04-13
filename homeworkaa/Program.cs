87afa130c0f550141876a0fcaab4fd2c79391e65
NazrinMammadova <102550859+NazrinMammadova@users.noreply.github.com>
2022-04-13 07:28:48 -0700

    added homework1

---
+++ homeworkaa/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace homeworkaa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 5;
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("verilmish eded cutdur");
+            }
+            else
+                Console.WriteLine("verilmish eded tekdir");
+
+        }
+    }
+    
+}
